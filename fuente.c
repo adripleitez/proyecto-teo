1d031f15f2db2f18669212489b894d1fce7f5c6e
adripleitez <sofpleitez@gmail.com>
2022-11-26 23:14:11 -0600

    new changes

--- fuente.c
+++ fuente.c
@@ -1,26 +1,25 @@
-struct myStructure {
-  int myNum;
-  char myLetter;
-};
-
 int main(){
-    Funcion2();
 	int a,b,c;
 	float d = 3.3;
 	a = 15;
 	b = 20;
 	c = a + b;
-	d= '_';
-	for (int i = 0; i < n; i--){
-	    while(n < 4){
-	        do{
-	            char ch = '';
-	        }
-	    }
+	d = '_';
+	n = 2;
+
+	for(int i = 0; i < n ; i++){
+        a = a + 1;
 	}
 
-	algo.myNum = 1;
-	algo.myLetter = '';
-	prueba(1, 2);
+	if(n == 2){
+	    a = 10;
+	}else{
+	    a = 23;
+	}
+
+    do {
+        n = n + 1;
+    } while ( n < 4);
+
 	return 0;
 }
