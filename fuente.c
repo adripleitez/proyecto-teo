d97132df0e54f99c9ba6dfe29b458a7cf8cbbc3e
OsEstrada <00064318@uca.edu.sv>
2022-11-29 18:05:28 -0600

    Se esperaba

--- fuente.c
+++ fuente.c
@@ -1,14 +1,17 @@
 int main(){
-	int a,b,c;
+    int a,b,c;
 	float d = 3.3;
 	a = 15;
 	b = 20;
 	c = a + b;
 	d = '_';
 	n = 2;
-
-	for(int i = 0; i < n ; i++){
-        a = a + 1;
+	for (int i = 0; i < n; i--){
+	    while(n < 4){
+	        do{
+	            char ch = '';
+	        } while(n < 4);
+	    }
 	}
 
 	if(n == 2){
@@ -17,9 +20,8 @@ int main(){
 	    a = 23;
 	}
 
-    do {
-        n = n + 1;
-    } while ( n < 4);
-
+	algo.myNum = 1;
+	algo.myLetter = ''
+	prueba(1, 2);
 	return 0;
 }
